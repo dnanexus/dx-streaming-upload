0fc35b2fa287dd30a2a380db91b4c43158fde7b0
Marek Hrvol <mhrvol-cf@dnanexus.com>
2021-08-09 16:15:26 +0200

    refactor

--- tests/test_incremental_upload.py
+++ tests/test_incremental_upload.py
@@ -3,6 +3,7 @@ import os
 import tempfile
 import shutil
 import pytest
+
 src_dir = os.path.join(os.path.dirname(__file__), "..")
 files_dir = os.path.join(src_dir, "files")
 sys.path.append(files_dir)
@@ -23,14 +24,16 @@ def create_files(rtacomplete_txt, rtacomplete_xml, copycomplete_txt):
             CopyComplete_txt.write("foobar")
     return tmp_folder
 
+
 # parametrized with ((RTAComplete.txt, RTAComplete.xml, CopyComplete.txt), result, result_novaseq)
 @pytest.mark.parametrize("permutation,result,result_novaseq", [((False, False, False), True, True), ((False, False, True), True, False),
-                                                               ((False, True, False), False, True), ((False, True, True), False, False), ((True, False, False), False, True),
-                                                               ((True, False, True), False, False), ((True, True, False), False, True), ((True, True, True), False, False)])
+                                                               ((False, True, False), False, True), ((False, True, True), False, False),
+                                                               ((True, False, False), False, True), ((True, False, True), False, False),
+                                                               ((True, True, False), False, True), ((True, True, True), False, False)])
 def test_is_still_uploading(permutation, result, result_novaseq):
     run_dir = create_files(*permutation)
     actual = iu.is_still_uploading(False, run_dir)
     actual_novaseq = iu.is_still_uploading(True, run_dir)
-    shutil.rmtree(run_dir)
+    shutil.rmtree(run_dir)  # deleting before potential assert failure
     assert actual == result
     assert actual_novaseq == result_novaseq
